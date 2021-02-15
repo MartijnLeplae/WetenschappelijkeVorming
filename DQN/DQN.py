219b6b1fa40f7b27ecb7a1d2c31d73cff9003b18
Martijn Leplae <martijn.leplae@student.leuven.be>
2021-02-15 21:41:30 +0100

    tests

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -32,9 +32,9 @@ state_size = env.observation_space.n  #shape[0]
 acion_size = env.action_space.n
 
 batch_size = 32  # Should be a power of 2
-n_episodes = 1001 # Number of games we want to play
+n_episodes = 1002 # Number of games we want to play
 
-history_size = 7 # 1 + nb of previous states to use
+history_size = 10 # 1 + nb of previous states to use
 dqn_input_vector_size = state_size*history_size  # what is the size of the inputlayer of the dqn
                                                  # This is the same as the history size*state_size if the representation
                                                  # only contains current and previous states. But it can differ
