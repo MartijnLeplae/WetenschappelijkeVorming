4f8e7effebe01feafe28a25eaf770cf4f1988fac
Martijn Leplae <martijn.leplae@student.leuven.be>
2021-02-15 19:19:57 +0100

    tests

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -32,7 +32,7 @@ state_size = env.observation_space.n  #shape[0]
 acion_size = env.action_space.n
 
 batch_size = 32  # Should be a power of 2
-n_episodes = 350 # Number of games we want to play
+n_episodes = 3 # Number of games we want to play
 
 history_size = 7 # 1 + nb of previous states to use
 dqn_input_vector_size = state_size*history_size  # what is the size of the inputlayer of the dqn
