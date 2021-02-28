f82e74ac8078b46890fd79d43d817743c963f174
Andreas <ahinderyckx@yahoo.co.uk>
2021-02-28 19:24:30 +0100

    Minor adjustments

--- DQN/gym-two_rooms/gym_two_rooms/envs/two_rooms_env.py
+++ DQN/gym-two_rooms/gym_two_rooms/envs/two_rooms_env.py
@@ -16,12 +16,6 @@ from rl.agents.dqn import DQNAgent
 from rl.policy import EpsGreedyQPolicy, LinearAnnealedPolicy
 from rl.memory import SequentialMemory
 
-"""
-- Bag of words
-- Intervallen van geheugen
-- Hybride 
-"""
-
 
 class TwoRoomsEnv(gym.Env):
     metadata = {'render.modes': ['human']}
@@ -46,7 +40,7 @@ class TwoRoomsEnv(gym.Env):
         self.state = []
         self.n_rooms = 2
         self.steps_taken = 0
-        # self.sequence = [1,1,1,0,1,0,1,1,1,1,0]
+        # self.sequence = [1,1,1,0,1,0,1,1,1,1,0]  # Example sequence with repeating subsequence
         self.sequence = [round(rnd.random()) for _ in range(10)]
         self.episode_length = len(self.sequence)  # Nb of actions in one episode
         self.history_length = 3
@@ -77,11 +71,6 @@ class TwoRoomsEnv(gym.Env):
 
         return self._get_state_repr(self.TOGGLE), reward, done, {}
 
-        # self.current_hist_rep = list(self.history)
-
-        # observation = (current room, last `history_length' nb. of  states)
-        # s = new_room, self.history[-self.history_length:]
-
     # This is the function where you decide what the agent (=neural network) can 'see'.
     # This can also include information about previous states (=history)
     """ 
@@ -92,8 +81,10 @@ class TwoRoomsEnv(gym.Env):
             hybrid representation: slider represents the percentage of the representation that is given in a bag of words
     @param: step: parameter to specify how many states should be left in-between the sampled history states
     """
-
     def _get_state_repr(self, slider=0, step=1):
+        if slider > 0:
+            assert self.history_length >= self.n_rooms
+
         # Take last `history_length' states; equal to self.state if history_length > len(self.state)
         # And take step parameter into account
         state_slice = self.state[-(self.history_length * step)::step]
@@ -101,7 +92,8 @@ class TwoRoomsEnv(gym.Env):
         ratio = math.floor(slider * len(state_slice))
         bag_part = state_slice[:ratio]
         counter = Counter(bag_part)
-        bag_of_words = [counter[0], counter[1]]
+        bag_of_words = [counter[i] for i in range(self.n_rooms)]
+        # bag_of_words = [counter[0], counter[1]]
         zeros = [0] * (self.history_length - len(state_slice) - len(bag_of_words))
         states = state_slice[-(self.history_length - len(bag_of_words)):]
         return np.array(bag_of_words + zeros + states)
@@ -118,15 +110,14 @@ class TwoRoomsEnv(gym.Env):
     def reset(self):
         self.steps_taken = 0
         self.state = []
-        # self.history = [0 for _ in range(self.history_length)]
-        # self.current_hist_rep = list(self.history)
 
         return self._get_state_repr(self.TOGGLE)
 
     def render(self, mode='human'):
         print('Current: ' + ' '.join(map(str, self.state[:-1])), end=' ')
         print('\u001b[31m' + str(self.state[-1]) + '\u001b[0m')  # Output last digit in the color red
-        print('Goal:    ' + ' '.join(map(str, self.sequence)))
+        if self.steps_taken == self.episode_length:
+            print('Goal:    ' + ' '.join(map(str, self.sequence)))
 
     def close(self):
         pass
