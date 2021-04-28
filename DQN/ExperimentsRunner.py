f7031d6336e49a2fff367973a52cfaa6ac873006
Andreas <ahinderyckx@yahoo.co.uk>
2021-04-28 22:40:32 +0200

    Fixed ExperimentsRunner

--- DQN/ExperimentsRunner.py
+++ DQN/ExperimentsRunner.py
@@ -7,9 +7,13 @@ Parameters = [
     [False, True, False, True],   # interval-bow
     [True, True, False, False]    # states-bow
 ]
-SEQUENCE = 121122
+CODE = '121122'
 
 for parameters in Parameters:
     for _ in range(10):
         trainer = Trainer('ButtonsWorld-v0')
-        trainer.env.set
+        n_states, bow, most_used, interval = parameters
+        trainer.env.set_user_parameters(n_states=n_states, bow=bow, most_used=most_used,
+                                        interval=interval, code=CODE)
+        trainer.start()
+        trainer.save_data()
