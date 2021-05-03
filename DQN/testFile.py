9c801b8eff10088c32b5deb0d084fce076493fac
Andreas Hinderyckx <andreas.hinderyckx@student.kuleuven.be>
2021-05-03 15:57:21 +0200

    Added incorrectly named data, changed testFile

--- DQN/testFile.py
+++ DQN/testFile.py
@@ -2,7 +2,7 @@ from DQN_keras_rl import Trainer
 
 for i in range(10):
     trainer = Trainer('TreasureMapHard-v0')
-    n_states, bow, most_used, interval, hist_sum = [True, False, True, False, False]
+    n_states, bow, most_used, interval, hist_sum = [True, False, False, True, False]
     trainer.env.set_user_parameters(N_STATES=n_states, BOW=bow, MOST_USED=most_used, INTERVAL=interval, HIST_SUM=hist_sum)
     if type(trainer.env.observation_space) == int:
         trainer.state_size = (trainer.env.observation_space,)  # (self.env.observation_space.n,)  #
@@ -10,6 +10,6 @@ for i in range(10):
         trainer.state_size = (trainer.env.observation_space.n,)
     trainer.start()
     try:
-        trainer.save_data(subdir='hist_sum1')
+        trainer.save_data(subdir='30/hist_sum1')
     except OSError:
         trainer.save_data()
