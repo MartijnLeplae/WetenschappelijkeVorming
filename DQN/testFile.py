377d82a51b964508fc85a24e3432dacf01912829
Andreas Hinderyckx <andreas.hinderyckx@student.kuleuven.be>
2021-05-03 20:20:22 +0200

    Added hist_sum, interval-states & most_used experiment data.

--- DQN/testFile.py
+++ DQN/testFile.py
@@ -2,7 +2,7 @@ from DQN_keras_rl import Trainer
 
 for i in range(10):
     trainer = Trainer('TreasureMapHard-v0')
-    n_states, bow, most_used, interval, hist_sum = [True, False, False, False, True]
+    n_states, bow, most_used, interval, hist_sum = [True, False, False, True, False]
     trainer.env.set_user_parameters(N_STATES=n_states, BOW=bow, MOST_USED=most_used, INTERVAL=interval, HIST_SUM=hist_sum)
     if type(trainer.env.observation_space) == int:
         trainer.state_size = (trainer.env.observation_space,)  # (self.env.observation_space.n,)  #
@@ -10,7 +10,7 @@ for i in range(10):
         trainer.state_size = (trainer.env.observation_space.n,)
     trainer.start()
     try:
-        trainer.save_data(subdir='30/hist_sum1')
+        trainer.save_data(subdir='30/interval-states1')
         print(f'N_STATES {trainer.env.N_STATES}, BOW: {trainer.env.BOW}, MU: {trainer.env.MOST_USED}, INTERVAL: {trainer.env.INTERVAL}, HIST_SUM: {trainer.env.HIST_SUM}')
     except OSError:
         trainer.save_data()
