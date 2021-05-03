f1fb0e4ecd8de5bb435328c69e6da737277b49f8
Andreas Hinderyckx <andreas.hinderyckx@student.kuleuven.be>
2021-05-03 14:24:30 +0200

    Added data, minor changes

--- DQN/experimentsRunner.py
+++ DQN/experimentsRunner.py
@@ -2,20 +2,21 @@ from DQN_keras_rl import Trainer
 
 # [n_states, bow, most_used, interval]
 Parameters = {
-    'interval1':     [False, False, False, True],  # interval
-    'bow1':          [False, True, False, False],  # bow
-    'interval-bow1': [False, True, False, True],   # interval-bow
-    'states1':       [True, False, False, False],  # states
-    'states-bow1':   [True, True, False, False]    # states-bow
+    'interval40':        [False, False, False, True, False],  # interval
+    'bow40':             [False, True, False, False, False],  # bow
+    'interval-bow40':    [False, True, False, True, False],   # interval-bow
+    'states40':          [True, False, False, False, False],  # states
+    'states-bow40':      [True, True, False, False, False],   # states-bow
+    'states-hist_sum40': [True, False, False, False, True]    # hist_sum
 }
 # CODE = '121'  # '121122212'  # '121122'  #   #
 
 for name, parameters in Parameters.items():
     for _ in range(10):
         trainer = Trainer('TreasureMapHard-v0')
-        n_states, bow, most_used, interval = parameters
+        n_states, bow, most_used, interval, hist_sum = parameters
         trainer.env.set_user_parameters(N_STATES=n_states, BOW=bow, MOST_USED=most_used,
-                                        INTERVAL=interval)
+                                        INTERVAL=interval, HIST_SUM=hist_sum)
         if type(trainer.env.observation_space) == int:
             trainer.state_size = (trainer.env.observation_space,)  # (self.env.observation_space.n,)  #
         else:
--- DQN/testFile.py
+++ DQN/testFile.py
@@ -1,6 +1,15 @@
 from DQN_keras_rl import Trainer
 
 for i in range(10):
-    trainer = Trainer('TreasureMap-v0')
+    trainer = Trainer('TreasureMapHard-v0')
+    n_states, bow, most_used, interval, hist_sum = [True, False, True, False, False]
+    trainer.env.set_user_parameters(N_STATES=n_states, BOW=bow, MOST_USED=most_used, INTERVAL=interval, HIST_SUM=hist_sum)
+    if type(trainer.env.observation_space) == int:
+        trainer.state_size = (trainer.env.observation_space,)  # (self.env.observation_space.n,)  #
+    else:
+        trainer.state_size = (trainer.env.observation_space.n,)
     trainer.start()
-    trainer.save_data()
+    try:
+        trainer.save_data(subdir='hist_sum1')
+    except OSError:
+        trainer.save_data()
