aed5ac06253aac2ea8a82df82a3b8c5dc15fed6b
Andreas Hinderyckx <andreas.hinderyckx@student.kuleuven.be>
2021-05-04 09:42:29 +0200

    Ran experimentsRunner for EpsLen 40, 50 & 60

--- DQN/gym-two_rooms/gym_two_rooms/envs/treasure_map_hard.py
+++ DQN/gym-two_rooms/gym_two_rooms/envs/treasure_map_hard.py
@@ -28,7 +28,7 @@ INEFFICIENTLY_SOLD_TREASURE = 4
 # History length
 NB_PREV_STATES = 3  # 3
 # Number of steps per episode
-EPISODE_LENGTH = 30  # 30  # 75
+EPISODE_LENGTH = 60  # 30  # 75
 
 
 class TreasureMapHardEnv(gym.Env):
